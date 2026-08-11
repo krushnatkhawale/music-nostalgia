4d15f0fbc0116825128f77ecaecdbd25d1ab455f
Krushnat <krushnatkhawale@gmail.com>
2026-08-11 23:57:24 +0530

    deploy: set Vite base to /music-nostalgia/ for GitHub Pages
    Co-authored-by: Krushnat <krushnatkhawale@gmail.com>
    Signed-off-by: Krushnat <krushnatkhawale@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // base must match the GitHub Pages sub-path for this repo
 export default defineConfig({
-  base: '/barbershop-radio/',
+  base: '/music-nostalgia/',
   plugins: [react()],
 })
